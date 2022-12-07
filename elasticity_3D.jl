d6a18ed243acee4b098b61bb0f40bd7f94b9378a
Alexandre Chen <napoleonalexandre@gmail.com>
2022-12-07 14:36:05 -0800

    removing formulation from a simple 3D poisson equation

--- elasticity_3D.jl
+++ elasticity_3D.jl
@@ -110,43 +110,6 @@ tau_z = 13/hz
 beta = -1
 
 
-# SBP-SAT Terms
-SAT_Left = tau_y * HI_Left * Left_operator'* Left_operator + beta * HI_Left * BS_Left' * Left_operator'*Left_operator
-SAT_Right = tau_y * HI_Right * Right_operator'* Right_operator + beta * HI_Right * BS_Right' * Right_operator'*Right_operator
-
-SAT_Top = tau_z * HI_Top  * Top_operator' * Top_operator * BS_Top
-SAT_Bottom = tau_z * HI_Bottom  * Bottom_operator' * Bottom_operator * BS_Bottom
-
-SAT_Front = tau_x * HI_Top * Front_operator' * Front_operator * BS_Front
-SAT_End = tau_x * HI_Bottom * End_operator' * End_operator * BS_End
-
-
-## To Be Completed
-SAT_Left_r = tau_y * HI_Left * Left_operator' + beta * HI_Left * BS_Left' * Left_operator'
-SAT_Right_r = tau_y * HI_Right * Right_operator'+ beta * HI_Right * BS_Right' * Right_operator'
-
-SAT_Top_r = tau_z * HI_Top  * Top_operator' 
-SAT_Bottom_r = tau_z * HI_Bottom * Bottom_operator'
-
-SAT_Front_r = tau_x * HI_Top * Front_operator'
-SAT_End_r = tau_x * HI_Bottom * End_operator'
-
-
-
-# Analytical solutions
-(analy_sol_3D,x_ex,y_ex,z_ex) = form_analy_sol(;N=N_x)
-# plot(x_ex,y_ex,analy_sol_3D[:,:,end],st=:surface)
-
-
-G_Left = [sin(π*i  + π*k) for i ∈ x_ex, k ∈ z_ex][:] # Dirichlet
-G_Right = [-sin(π*i  + π*k) for i ∈ x_ex, k ∈ z_ex][:] # Dirichlet
-G_Bottom = [-π*cos(π*i + π*j) for i ∈ x_ex, j ∈ y_ex][:] # norm vector -1 Neumann
-G_Top = [π*cos(π*i + π*j + π) for i ∈ x_ex, j ∈ y_ex][:] # sin(pi) = -1 Neumann
-G_Front = [π*cos(π*j + π*k + π) for j ∈ y_ex, k ∈ z_ex][:] # Neumann
-G_End = [-π*cos(π*j + π*k) for j ∈ y_ex, k ∈ z_ex][:] # Neumann
-
-source_terms = 3π^2 * analy_sol_3D;
-
 
 # A = -(D2_x + D2_y + D2_z) + SAT_Front + SAT_End + SAT_Left + SAT_Right + SAT_Bottom + SAT_Top
 
@@ -156,7 +119,8 @@ source_terms = 3π^2 * analy_sol_3D;
 # numerical_sol = A\b
 
 # numerical_sol_3D = reshape(numerical_sol,N_x+1,N_y+1,N_z+1)
-# analy_sol_3D
+
+# analy_sol_3D u1 = u2 = u3 = sin(πx + πy + πz)
 
 u1_filter = get_u1(Nx,Ny,Nz)
 u2_filter = get_u2(Nx,Ny,Nz)
