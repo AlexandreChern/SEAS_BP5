c48a8b8400c5fe7a80e124c984e76433c4be6df7
Alexandre Chen <napoleonalexandre@gmail.com>
2023-02-08 22:42:15 -0800

    replacing notations in source_terms.jl with _s

--- source_terms.jl
+++ source_terms.jl
@@ -1,22 +1,22 @@
 using Symbolics
 
-@variables x y z
-@variables K μ
+@variables x_s y_s z_s
+@variables K_s μ_s
 @variables π_s # = Symbolics.pi
 
-u1 = cos(2*π_s*x) * cos(π_s*y) * cos(π_s*z)
-u2 = cos(π_s*x) * cos(3*π_s*y) * cos(π_s*z)
-u3 = cos(π_s*x) * cos(π_s*y) * cos(π_s*4*z)
+u1_s = cos(2 * π_s * x_s) * cos(π_s * y_s) * cos(π_s * z_s)
+u2_s = cos(π_s * x_s) * cos(3 * π_s * y_s) * cos(π_s * z_s)
+u3_s = cos(π_s * x_s) * cos(π_s * y_s) * cos(π_s * 4 * z_s)
 
 # First derivatives
-Dx = Differential(x)
-Dy = Differential(y)
-Dz = Differential(z)
+Dx = Differential(x_s)
+Dy = Differential(y_s)
+Dz = Differential(z_s)
 
 # Second derivatives
-Dxx = Differential(x)^2
-Dyy = Differential(y)^2
-Dzz = Differential(z)^2
+Dxx = Differential(x_s)^2
+Dyy = Differential(y_s)^2
+Dzz = Differential(z_s)^2
 
 Dxy = Dx * Dy
 Dxz = Dx * Dz
@@ -26,17 +26,17 @@ Dzx = Dxz
 Dzy = Dyz
 Dyx = Dxy
 
-Dx_u1 = expand_derivatives(Dx(u1)) 
-Dx_u2 = expand_derivatives(Dy(u1)) 
+Dx_u1 = expand_derivatives(Dx(u1_s)) 
+Dx_u2 = expand_derivatives(Dy(u1_s)) 
 
-Dxy_u1 = expand_derivatives(Dxy(u1)) 
-Dyx_u1 = expand_derivatives(Dyx(u1)) 
-Dyz_u1 = expand_derivatives(Dyz(u1))
-Dzx_u1 = expand_derivatives(Dzx(u2))
+Dxy_u1 = expand_derivatives(Dxy(u1_s)) 
+Dyx_u1 = expand_derivatives(Dyx(u1_s)) 
+Dyz_u1 = expand_derivatives(Dyz(u1_s))
+Dzx_u1 = expand_derivatives(Dzx(u2_s))
 
 
-expand_derivatives(Dxx(u1)) 
-expand_derivatives(Dyy(u1))
+expand_derivatives(Dxx(u1_s)) 
+expand_derivatives(Dyy(u1_s))
 
 # substitute(Dxy_u1,Dict(π_s=>π))
 
@@ -46,18 +46,18 @@ expand_derivatives(Dyy(u1))
 # Equation for u1
 
 u1_source = expand_derivatives(
-        (K - 2//3 * μ) * (Dxx(u1) + Dxy(u2) + Dxz(u3)) 
-    +   2 * μ * Dxx(u1)
+        (K_s - 2//3 * μ_s) * (Dxx(u1_s) + Dxy(u2_s) + Dxz(u3_s)) 
+    +   2 * μ_s * Dxx(u1_s)
     +   μ * (Dyy(u1) + Dyx(u2))
     +   μ * (Dzz(u3) + Dzx(u3))
 )
 
-substitute(u1_source, Dict(K=>1,μ=>1))
+substitute(u1_source, Dict(K=>1,μ=>1)) # how to substitute symbolics with real values
 
 u2_source = expand_derivatives(
         μ * (Dxx(u2) + Dxy(u1))
     +   (K - 2//3 * μ) * (Dyx(u1) + Dyy(u2) + Dyz(u3))
-    +   2μ * (Dyy(u2))
+    +   2 * μ * (Dyy(u2))
     +   μ * (Dzz(u2) + Dzy(u3))
 )
 
@@ -66,5 +66,6 @@ substitute(u2_source, Dict(K=>1,μ=>1))
 u3_source = expand_derivatives(
         μ * (Dxx(u3) + Dxz(u1))
     +   μ * (Dyy(u3) + Dyz(u2))
-    +   (K - 2//3 * μ) 
+    +   (K - 2//3 * μ) * (Dzx(u1) + Dzy(u2) + Dzz(u3))
+    +   2 * μ * (Dzz(u3))
 )
