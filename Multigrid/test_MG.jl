b2521419df91c047835327e62ede243201814f20
Alexandre Chen <napoleonalexandre@gmail.com>
2023-11-10 22:48:46 -0800

    exploring proper interpolation for 3D linear elasticity

--- Multigrid/test_MG.jl
+++ Multigrid/test_MG.jl
@@ -22,3 +22,48 @@ dot(mg_struct_CUDA.r_CUDA[1],mg_struct_CUDA.r_CUDA[1]) / dot(mg_struct_CUDA.r_CU
 
 
 dot(mg_struct_CUDA.b_mg[1],mg_struct_CUDA.b_mg[1]) / dot(mg_struct_CUDA.b_mg[1], mg_struct_CUDA.A_mg[1] * mg_struct_CUDA.b_mg[1])
+
+mg_struct_CUDA
+
+
+
+
+
+# exploring interpolation operators
+
+N = 16
+N_xh = N_yh = N_zh = N
+hx_h = 1 / N_xh
+hy_h = 1 / N_yh
+hz_h = 1 / N_zh
+
+N_2h = div(N,2)
+N_x2h = N_y2h = N_z2h = N_2h
+hx_2h = 1 / N_x2h
+hy_2h = 1 / N_y2h
+hz_2h = 1 / N_z2h
+
+
+
+M_h, RHS_h, H_tilde_h, HI_tilde_h, analy_sol_h = Assembling_3D_matrices(N_xh, N_yh, N_zh;p=2)
+M_2h, RHS_2h, H_tilde_2h, HI_tilde_2h, analy_sol_2h = Assembling_3D_matrices(N_x2h, N_y2h, N_z2h;p=2)
+
+
+RHS_h_1_reshaped = reshape(u1_filter_MF(RHS_h), N_xh + 1, N_yh + 1, N_zh + 1)
+RHS_2h_1_reshaped = reshape(u1_filter_MF(RHS_2h), N_x2h + 1, N_y2h + 1, N_z2h + 1)
+
+
+RHS_h_1_reshaped = reshape(HI_tilde_h * u1_filter_MF(RHS_h), N_xh + 1, N_yh + 1, N_zh + 1)
+RHS_2h_1_reshaped = reshape(HI_tilde_2h * u1_filter_MF(RHS_2h), N_x2h + 1, N_y2h + 1, N_z2h + 1)
+
+RHS_h_1_reshaped[1,:,:]
+RHS_2h_1_reshaped[1,:,:]
+
+rest_h = restriction_matrix_v0(N_xh,N_yh,N_zh,N_x2h,N_y2h,N_z2h) 
+RHS_restricted = H_tilde_2h * rest_h * HI_tilde_h * u1_filter_MF(RHS_h)
+
+RHS_resricted_reshaped = reshape(RHS_restricted,N_x2h + 1, N_y2h + 1, N_z2h + 1) / 2
+reshape(u1_filter_MF(RHS_2h), N_x2h + 1, N_y2h + 1, N_z2h + 1)
+
+plot(0:hx_h:1,0:hy_h:1, RHS_h_1_reshaped[1,:,:], st=:surface)
+plot(0:hx_2h:1,0:hy_2h:1, RHS_2h_1_reshaped[1,:,:], st=:surface)
