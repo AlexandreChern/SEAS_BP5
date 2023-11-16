77811cbb8235c5277469167870f687c2cc3ef933
Alexandre Chen <napoleonalexandre@gmail.com>
2023-11-15 22:42:20 -0800

    adding functions to calculate f(V,\theta) and a(x2,x3)

--- Simulations/BP5-QD.jl
+++ Simulations/BP5-QD.jl
@@ -1,7 +1,38 @@
 include("helper.jl")
+include("coefficients.jl")
+
 
 function main()
-    sim_years = 1000
+    if @isdefined BP5_coeff
+        println("BP5 coefficients defined, using defined values")
+    else
+        println("BP5 coefficients not defined, defining it now")
+        BP5_coeff = coefficients(
+            2670,
+            3.464,
+            0.25,
+            0.004,
+            0.04,
+            0.03,
+            25,
+            0.14,
+            1E-9,
+            1E-9,
+            1E-6,
+            0.6,
+            2,
+            2,
+            12,
+            60,
+            40,
+            100,
+            12,
+            1000,
+            1800
+        )
+    end
+
+    
 
     
 end
--- Simulations/helper.jl
+++ Simulations/helper.jl
@@ -10,11 +10,49 @@ function odefun(dψV, ψδ, p, t)
 end
 
 struct coefficients
-    #       # variable names                # recommended values
-    Vp      # plate rate                    # recommended values
-    ρ       # density
-    cs      # shear wave speed
-    a0      # rate-and-state parameter
-    amax    # rate-and-state parameter
-    b0      # rate-and-state parameter
+    # Table 1: Parameter values used in this benchmark problem 
+            # variable names                        # recommended values                
+    ρ       # density                               2670 kg/m³
+    cs      # shear wave speed                      3.464 km/s
+    ν       # Poisson's ratio                       0.25
+    a0      # rate-and-state parameter              0.004
+    amax    # rate-and-state parameter              0.04
+    b0      # rate-and-state parameter              0.03
+    σn      # effective normal stress               25 MPa
+    L       # critical slip distance                0.14 m/0.13 m 
+    Vp      # plate rate                            10⁻⁹ m/s
+    Vinit   # initial slip rate                     10⁻⁹ m/s
+    V0      # reference slip rate                   10⁻⁶ m/s 
+    f0      # reference friction coefficient        0.6
+    hs      # width of shallow VS zone              2 km
+    ht      # width of VW-VS transition zone        2 km
+    H       # width of uniform VW region            12 km
+    l       # length of uniform VW region           60 km
+    Wf      # width of rate-and-state fault         40 km
+    lf      # length of rate-and-state fault        100 km
+    w       # width of favorible nucleation zone    12 km
+    Δz      # suggested cell size                   1000m
+    tf      # final simulation time                 1800 years
 end
+
+
+
+function f_func(V, θ, a, BP5_coeff::coefficients)
+    return a * asinh(
+        V / (2 * BP5_coeff.V0) * exp((BP5_coeff.f0 + BP5_coeff.b0 * ln(BP5_coeff.V0 / BP5_coeff.L) ) / a)
+    )
+end
+
+function a_func(x2, x3, BP5_coeff::coefficients)
+    if (BP5_coeff.hs + BP5_coeff.ht ≤ x3 ≤ BP5_coeff.hs + BP5_coeff.ht + BP5_coeff.H) && (abs(x2) ≤ BP5_coeff.l / 2)
+        return BP5_coeff.a0
+    elseif (0 ≤ x3 ≤ BP5_coeff.hs) || (BP5_coeff.hs + 2 * BP5_coeff.ht + BP5_coeff.H ≤ x3 ≤ BP5_coeff.Wf)
+        return BP5_coeff.amax
+    else
+        r = max(abs(x3 - BP5_coeff.hs - BP5_coeff.ht - BP5_coeff.H/2) - BP5_coeff.H/2, abs(x2) - BP5_coeff.l/2) / BP5_coeff.ht
+        return BP5_coeff.a0 + r * (BP5_coeff.amax - BP5_coeff.a0)
+    end
+end
+
+
+a_func(20,20,BP5_coeff)
