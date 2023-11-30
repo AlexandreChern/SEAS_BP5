02cd124429967a5dd12306223803c1b1f9d016d8
Alexandre Chen <napoleonalexandre@gmail.com>
2023-11-30 09:43:25 -0800

    changing code to use defualt constructor

--- Simulations/BP5-QD.jl
+++ Simulations/BP5-QD.jl
@@ -17,29 +17,7 @@ function main()
         println("BP5 coefficients defined, using defined values")
     else
         println("BP5 coefficients not defined, defining it now")
-        BP5_coeff = coefficients(
-            2670,                   # ρ
-            3.464,                  # cs
-            0.25,                   # ν
-            0.004,                  # a0
-            0.04,                   # amax
-            0.03,                   # b0            value for b in this problem
-            25,                     # σn
-            0.14,                   # L
-            1E-9,                   # Vp
-            1E-9,                   # Vinit
-            1E-6,                   # V0
-            0.6,                    # f0
-            2,                      # hs
-            2,                      # ht
-            12,                     # H
-            60,                     # l
-            40,                     # Wf
-            100,                    # lf
-            12,                     # w
-            1000,                   # Δz
-            1800                    # tf
-        )
+        BP5_coeff = coefficients() # calling default constructor
     end
 
     # setting b = b0
--- Simulations/helper.jl
+++ Simulations/helper.jl
@@ -47,6 +47,31 @@ struct coefficients
     tf      # final simulation time                 1800 years
 end
 
+coefficients() = coefficients(
+    2670,                   # ρ
+    3.464,                  # cs
+    0.25,                   # ν
+    0.004,                  # a0
+    0.04,                   # amax
+    0.03,                   # b0            value for b in this problem
+    25,                     # σn
+    0.14,                   # L
+    1E-9,                   # Vp
+    1E-9,                   # Vinit
+    1E-6,                   # V0
+    0.6,                    # f0
+    2,                      # hs
+    2,                      # ht
+    12,                     # H
+    60,                     # l
+    40,                     # Wf
+    100,                    # lf
+    12,                     # w
+    1000,                   # Δz
+    1800                    # tf
+)
+
+
 # for BP5, b is set to be the constant value b0
 # using b in the variables of the functions below 
 # to write more modular code easier to maintain
