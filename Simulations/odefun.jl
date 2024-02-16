e82fe34ea851112d761094b85f911898896e202a
Alexandre Chen <napoleonalexandre@gmail.com>
2024-02-15 22:28:59 -0800

    adding bounding to odefun

--- Simulations/odefun.jl
+++ Simulations/odefun.jl
@@ -108,112 +108,117 @@ function odefun(dψV, ψδ, odeparam, t)
     # If reject return
     if reject_step[1]
         return
-    end
-    # End if reject block
+    else
+        # End if reject block
 
-    @show t
+        @show t
 
-   
-
-    ## Setting up ratees of change for state and slip
-
-    # ψ = @view ψδ[1:(fN2 + 1)*(fN3 + 1)]
-    # δ = @view ψδ[(fN2 + 1)*(fN3 + 1) + 1:end]
-
-    # dψ = @view dψV[1:(fN2 + 1)*(fN3 + 1)]
-    # V = @view dψV[(fN2 + 1)*(fN3 + 1) + 1:end]
-
-    dψ, V, ψ, δ = create_view(dψV, ψδ) # creating "views" to get dψ, V, ψ, δ
-
-    dψ .= 0;
-    V .= 0;
-    ## End setting up dψV and ψδ
-
-    # Updating RHS using δ
-    RHS .= 0 # Resetting RHS
-    RHS .+= updators[1] * δ[1:2:end] ./ 2 # divide slip by half to get displacements
-    RHS .+= updators[2] * δ[2:2:end] ./ 2
-
-    # Updating RHS using remote loading for face 2 for V2
-    RHS .+= updators[3] * (fill(t .* Vp / 2, div(length(δ),2)))
-    # Updating RHS using remote loading for face 2 for V3
-    # RSH .+= updators[3] * (fill(0, div(length(δ),2)))
-
-    # End updating RHS using δ
-    abstol_ = norm(RHS) * sqrt(eps(Float64))
-
-    # Solving linear system using iterative methods
-    # u_iterative, history = cg(M_GPU, CuArray(RHS), log=true);    # solving with non preconditioned cg
-    u_iterative, history = cg!(CuArray(u),M_GPU, CuArray(RHS), abstol=abstol_, log=true);    # solving with non preconditioned cg
-
-    # this can be replaced with MGCG in future 
-    # @show history.iters
-
-    if typeof(u_iterative) == CuArray{Float64, 1, CUDA.Mem.DeviceBuffer}
-    u_iterative = Array(u_iterative)
-    end
-
-    # u[:] .= Array(u_iterative);
-    u[:] .= u_iterative
-    # End of solving 
-
-    # updating values TODO
-    # computate tractions using u: similar to calculation in 
-    # assembling 3D matrices
-  
-
-    # do non-linear solve for corresponding slip rate TODO
-    # given traction on the fault and state variables   
-    # check https://github.com/Thrase/Thrase.jl/blob/main/src/odefun.jl
     
-    # update rate-and-state region
-    # update rate-and-state region using slip rate 
-    # and update the region out of rate-and-state 
-    # using steady state slip rate
 
-    # len_τ = div(length(Δτb), 2)
-    Δτ = @view Δτb[1:2:length(Δτb)] 
-    Δτz = @view Δτb[2:2:length(Δτb)]
+        ## Setting up ratees of change for state and slip
 
-    τ0 = @view τb[1:2:length(τb)]
-    τz0 =  @view τb[2:2:length(τb)]
+        # ψ = @view ψδ[1:(fN2 + 1)*(fN3 + 1)]
+        # δ = @view ψδ[(fN2 + 1)*(fN3 + 1) + 1:end]
 
-    # Δτz .= compute_traction_τz() # TODO
-    Δτ .= Face_operators[1] * sigma_21 * u_iterative
-    Δτz .= Face_operators[1] * sigma_31 * u_iterative
+        # dψ = @view dψV[1:(fN2 + 1)*(fN3 + 1)]
+        # V = @view dψV[(fN2 + 1)*(fN3 + 1) + 1:end]
+
+        dψ, V, ψ, δ = create_view(dψV, ψδ) # creating "views" to get dψ, V, ψ, δ
+
+        dψ .= 0;
+        V .= 0;
+        ## End setting up dψV and ψδ
+
+        # Updating RHS using δ
+        RHS .= 0 # Resetting RHS
+        RHS .+= updators[1] * δ[1:2:end] ./ 2 # divide slip by half to get displacements
+        RHS .+= updators[2] * δ[2:2:end] ./ 2
+
+        # Updating RHS using remote loading for face 2 for V2
+        RHS .+= updators[3] * (fill(t .* Vp / 2, div(length(δ),2)))
+        # Updating RHS using remote loading for face 2 for V3
+        # RSH .+= updators[3] * (fill(0, div(length(δ),2)))
+
+        # End updating RHS using δ
+        abstol_ = norm(RHS) * sqrt(eps(Float64))
+
+        # Solving linear system using iterative methods
+        # u_iterative, history = cg(M_GPU, CuArray(RHS), log=true);    # solving with non preconditioned cg
+        u_iterative, history = cg!(CuArray(u),M_GPU, CuArray(RHS), abstol=abstol_, log=true);    # solving with non preconditioned cg
+
+        # this can be replaced with MGCG in future 
+        # @show history.iters
+
+        if typeof(u_iterative) == CuArray{Float64, 1, CUDA.Mem.DeviceBuffer}
+        u_iterative = Array(u_iterative)
+        end
+
+        # u[:] .= Array(u_iterative);
+        u[:] .= u_iterative
+        # End of solving 
+
+        # updating values TODO
+        # computate tractions using u: similar to calculation in 
+        # assembling 3D matrices
     
-    # TODO use previously solved solutions
-    # Vn1 = 1e-10 # use very small values 
-    # Vn2 = 1e-10 # use very small values
 
-    # Vn1_v = fill(Vn1, fN2 * fN3)
-    # Vn2_v = fill(Vn2, fN2 * fN3)
+        # do non-linear solve for corresponding slip rate TODO
+        # given traction on the fault and state variables   
+        # check https://github.com/Thrase/Thrase.jl/blob/main/src/odefun.jl
+        
+        # update rate-and-state region
+        # update rate-and-state region using slip rate 
+        # and update the region out of rate-and-state 
+        # using steady state slip rate
 
-    odeparam.τfb .= τb .+ Δτb 
+        # len_τ = div(length(Δτb), 2)
+        Δτ = @view Δτb[1:2:length(Δτb)] 
+        Δτz = @view Δτb[2:2:length(Δτb)]
 
-    τ2 = (τ0 + Δτ)[RS_filter_2D_nzind]
-    τ3 = (τz0 + Δτz)[RS_filter_2D_nzind]
+        τ0 = @view τb[1:2:length(τb)]
+        τz0 =  @view τb[2:2:length(τb)]
 
-    # (f_v, g_v, dfx_v, dfy_v, dgx_v, dgy_v) = rateandstate_vectorized(V2_v, V3_v, ψ, σn, τ2, τ3, η, RSas, RSV0)
-    (V2_tmp, V3_tmp, _, _, iter) = newtbndv_vectorized(rateandstate_vectorized, V2_v, V3_v, ψ, σn, Vector(τ2), Vector(τ3), η, RSas, RSV0; ftol=1e-12, maxiter=500, atolx=1e-10, rtolx=1e-10)
-    # be careful of the order of the parameters
-    V2_v .= V2_tmp
-    V3_v .= V3_tmp
-    @show iter
+        # Δτz .= compute_traction_τz() # TODO
+        Δτ .= Face_operators[1] * sigma_21 * u_iterative
+        Δτz .= Face_operators[1] * sigma_31 * u_iterative
+        
+        # TODO use previously solved solutions
+        # Vn1 = 1e-10 # use very small values 
+        # Vn2 = 1e-10 # use very small values
 
-    # out side of RS, V2 = Vp, V3 = 0
-    V[1:2:end] .= Vp 
-    V[2:2:end] .= 0
+        # Vn1_v = fill(Vn1, fN2 * fN3)
+        # Vn2_v = fill(Vn2, fN2 * fN3)
 
-    # inside RS, set V2 and V3 with solutions
-    # V[2 .* RS_filter_2D_nzind .- 1] .= V2_v
-    # V[2 .* RS_filter_2D_nzind] .= V3_v
+        odeparam.τfb .= τb .+ Δτb 
 
-    V[2 .* RS_filter_2D_nzind .- 1] .= V2_v
-    V[2 .* RS_filter_2D_nzind] .= V3_v
-    # Update ψ
-    # dψ[n] = (RSb * RSV0 / RSDc) * (exp((RSf0 - ψn) / RSb) - abs(Vn) / RSV0) # BP1
-    # dψ .= (RSb * RSV0 / RSL) .* (exp.((RSf0 .- ψ) ./ RSb) .- sqrt.(V2_v.^2 .+ V3_v.^2) ./ RSV0)
-    dψ .= (RSb * RSV0 ./ RSLs) .* (exp.((RSf0 .- ψ) ./ RSb) .- sqrt.(V2_v.^2 .+ V3_v.^2) ./ RSV0)
+        τ2 = (τ0 + Δτ)[RS_filter_2D_nzind]
+        τ3 = (τz0 + Δτz)[RS_filter_2D_nzind]
+
+        # (f_v, g_v, dfx_v, dfy_v, dgx_v, dgy_v) = rateandstate_vectorized(V2_v, V3_v, ψ, σn, τ2, τ3, η, RSas, RSV0)
+        (V2_tmp, V3_tmp, _, _, iter) = newtbndv_vectorized(rateandstate_vectorized, V2_v, V3_v, ψ, σn, Vector(τ2), Vector(τ3), η, RSas, RSV0; ftol=1e-12, maxiter=500, atolx=1e-10, rtolx=1e-10)
+        # be careful of the order of the parameters
+        if !all(isfinite.(V2_tmp)) or !all(isfinite.(V3_tmp))
+            reject_step[1] = true
+            return
+        end
+        V2_v .= V2_tmp
+        V3_v .= V3_tmp
+        @show iter
+
+        # out side of RS, V2 = Vp, V3 = 0
+        V[1:2:end] .= Vp 
+        V[2:2:end] .= 0
+
+        # inside RS, set V2 and V3 with solutions
+        # V[2 .* RS_filter_2D_nzind .- 1] .= V2_v
+        # V[2 .* RS_filter_2D_nzind] .= V3_v
+
+        V[2 .* RS_filter_2D_nzind .- 1] .= V2_v
+        V[2 .* RS_filter_2D_nzind] .= V3_v
+        # Update ψ
+        # dψ[n] = (RSb * RSV0 / RSDc) * (exp((RSf0 - ψn) / RSb) - abs(Vn) / RSV0) # BP1
+        # dψ .= (RSb * RSV0 / RSL) .* (exp.((RSf0 .- ψ) ./ RSb) .- sqrt.(V2_v.^2 .+ V3_v.^2) ./ RSV0)
+        dψ .= (RSb * RSV0 ./ RSLs) .* (exp.((RSf0 .- ψ) ./ RSb) .- sqrt.(V2_v.^2 .+ V3_v.^2) ./ RSV0)
+    end
     nothing
 end
